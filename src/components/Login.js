a3ed9f2b495b90bcfa3a57f4a9080359472f93f8
Nitin <nitinbhardwaj0500@gmail.com>
2024-03-10 23:53:05 +0530

    feedback form js+ modal close button

--- src/components/Login.js
+++ src/components/Login.js
@@ -91,6 +91,12 @@ export default function Login() {
                   <h3 className="text-3xl font-semibold text-center justify-center">
                     Login
                   </h3>
+                  <button onClick={() => setShowModal(false)} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-right items-left dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                  </button>
                 </div>
                 {/*body*/}
                 <div className="flex justify-center items-center">
