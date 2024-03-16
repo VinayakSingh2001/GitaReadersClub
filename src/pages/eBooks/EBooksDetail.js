4003a3a97b33ee6bd815011215eeeabf7572af9b
naresh <vishnoinaresh2@gmail.com>
2024-03-16 12:56:03 +0530

    Ebook question answer stored in database

--- src/pages/eBooks/EBooksDetail.js
+++ src/pages/eBooks/EBooksDetail.js
@@ -4,8 +4,8 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { TreeView } from "@mui/x-tree-view/TreeView";
 import { TreeItem } from "@mui/x-tree-view/TreeItem";
-import { get, getDatabase, ref } from "firebase/database";
-import { app } from "../../firebase.config";
+import { get, getDatabase, ref, set } from "firebase/database";
+import { app, auth } from "../../firebase.config";
 import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import { toast } from "react-toastify";
 import { Fullscreen } from "@mui/icons-material";
@@ -15,8 +15,10 @@ function EbookDetail() {
   const [selectedBook, setSelectedBook] = useState(null);
   const [selectedChapter, setSelectedChapter] = useState(null);
   const [selectedArticle, setSelectedArticle] = useState(null);
-  const [selectedQuestion, setSelectedQuestion] = useState(null);
-  const [selectedQuestionDetails, setSelectedQuestionDetails] = useState(null);
+  const [selectedQuestions, setSelectedQuestions] = useState(null);
+  // const [selectedQuestionDetails, setSelectedQuestionDetails] = useState(null);
+  const [selectedChapterId, setSelectedChapterId] = useState(null);
+  const [selectedArticleId, setSelectedArticleId] = useState(null);
   const [openArticles, setOpenArticles] = useState({});
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   useEffect(() => {
@@ -34,7 +36,7 @@ function EbookDetail() {
         console.error(error);
       }
     };
-
+   
     fetchBooks();
   }, []);
 
@@ -43,17 +45,18 @@ function EbookDetail() {
     setSelectedBook(book);
     setSelectedChapter(null);
     setSelectedArticle(null);
-    setSelectedQuestion(null);
-    setSelectedQuestionDetails(null);
+    // setSelectedQuestion(null);
+    // setSelectedQuestionDetails(null);
     setOpenArticles({}); // Reset open articles when selecting a new book
   };
 
   const handleChapterSelect = (chapterId) => {
     const chapter = selectedBook.ChapterId[chapterId];
+    setSelectedChapterId(chapterId);
     setSelectedChapter(chapter);
     setSelectedArticle(null);
-    setSelectedQuestion(null);
-    setSelectedQuestionDetails(null);
+    // setSelectedQuestion(null);
+    // setSelectedQuestionDetails(null);
     setOpenArticles({}); // Reset open articles when selecting a new chapter
   };
 
@@ -63,18 +66,19 @@ function EbookDetail() {
       [chapterId]: articleId,
     }));
     const article = selectedBook.ChapterId[chapterId].ArticleId[articleId];
+    setSelectedArticleId(articleId)
     setSelectedArticle(article);
-    setSelectedQuestion(null);
-    setSelectedQuestionDetails(null);
+    // setSelectedQuestion(null);
+    // setSelectedQuestionDetails(null);
   };
 
-  const handleQuestionSelect = (questionId) => {
-    if (selectedArticle && selectedArticle.QuestionId) {
-      const question = selectedArticle.QuestionId[questionId];
-      setSelectedQuestion(question);
-      setSelectedQuestionDetails(question);
-    }
-  };
+  // const handleQuestionSelect = (questionId) => {
+  //   if (selectedArticle && selectedArticle.QuestionId) {
+  //     const question = selectedArticle.QuestionId[questionId];
+  //     setSelectedQuestion(question);
+  //     // setSelectedQuestionDetails(question);
+  //   }
+  // };
 
   const renderChapterTree = () => {
     if (!selectedBook || !selectedBook.ChapterId) return null;
@@ -133,20 +137,25 @@ function EbookDetail() {
       <TreeItem
         nodeId={`${chapterId}${articleId}-questions`}
         label="Questions"
-        onClick={(e) => e.preventDefault()} // Prevent default action for this node
+        onClick={(e) => {
+          e.preventDefault();
+          console.log(questions)
+          setSelectedQuestions(questions)
+        }} // Prevent default action for this node
       >
-        <select onChange={(e) => handleQuestionSelect(e.target.value)}>
+        {/* <select onChange={(e) => handleQuestionSelect(e.target.value)}>
           <option value="">Select a question</option>
           {Object.keys(questions).map((questionId) => (
             <option key={questionId} value={questionId}>
               {`Question ID: ${questionId}`}
             </option>
           ))}
-        </select>
+        </select> */}
       </TreeItem>
     );
   };
   const handleChange = (optionId) => {
+    console.log(optionId)
     if (selectedAnswer === optionId) {
       setSelectedAnswer(null);
     } else {
@@ -154,14 +163,37 @@ function EbookDetail() {
     }
   };
 
-  const handleAnswerSubmit = async (e) => {
-    e.preventDefault();
-    if (selectedQuestion.Answer === selectedAnswer) {
-      toast.success("Right Answer");
-    } else {
-      toast.error("wrong Answer");
+  const handleAnswerSubmit =  async(questionId,answerId) => {
+    // e.preventDefault();
+    console.log(answerId)
+    console.log(selectedChapterId);
+    if(selectedAnswer[0] === answerId){
+      toast.success("Correct Answer");
+      const db = getDatabase(app);
+      const user = auth.currentUser;
+      if(user){
+        const userId = user.uid;
+       const dbRef = ref(db,`progress/${userId}/${selectedChapterId}/${selectedArticleId}/${questionId}`);
+       await set(dbRef,1);
+      }
+      
     }
-    setSelectedAnswer(null);
+    else{
+      toast.error("Wrong Answer");
+      const db = getDatabase(app);
+      const user = auth.currentUser;
+      if(user){
+        const userId = user.uid;
+       const dbRef = ref(db,`progress/${userId}/${selectedChapterId}/${selectedArticleId}/${questionId}`);
+       await set(dbRef,0);
+      }
+    }
+    // if (selectedQuestion.Answer === selectedAnswer) {
+    //   toast.success("Right Answer");
+    // } else {
+    //   toast.error("wrong Answer");
+    // }
+    // setSelectedAnswer(null);
   };
   return (
     <div className="container mx-auto px-4 py-8">
@@ -188,7 +220,47 @@ function EbookDetail() {
           </Box>
         </div>
         <div className="lg:w-3/4 pl-0 lg:pl-4">
-          {selectedQuestionDetails ? (
+         {selectedQuestions && Object.keys(selectedQuestions).map((selectedQuestionId,index)=>{
+          const selectedQuestionDetails = selectedQuestions[selectedQuestionId];
+             return (<div
+              style={{
+                width: Fullscreen,
+                backgroundColor: "#f9f9f9",
+                padding: "16px",
+              }}
+            >
+              <h4 className="text-2xl font-semibold py-2">Selected Question</h4>
+              <p className="text-xl">{selectedQuestionDetails.Question}</p>
+
+              <ul>
+                {Object.entries(selectedQuestionDetails.Options).map(
+                  ([optionId, optionText]) => (
+                    <li key={optionId} className="pl-2 pt-2 text-xl">
+                      <label htmlFor={optionId}>
+                        <input
+                          type="radio"
+                          id={index+optionId}
+                          checked={selectedAnswer === optionId+index}
+                          onClick={() => handleChange(optionId+index)}
+                        />
+                        {optionText}
+                      </label>
+                    </li>
+                  )
+                )}
+              </ul>
+
+              <div className="flex pt-4 pb-4">
+                <button
+                  className="bg-blue-200 hover:scale-105 transition-transform  p-2 rounded-lg ml-6"
+                  onClick={()=>handleAnswerSubmit(selectedQuestionId,selectedQuestionDetails.Answer)}
+                >
+                  submit
+                </button>
+              </div>
+            </div>)
+         })}
+          {/* {selectedQuestionDetails ? (
             <div
               style={{
                 width: Fullscreen,
@@ -229,9 +301,9 @@ function EbookDetail() {
           ) : selectedArticle && selectedArticle.Data ? (
             <div className=" bg-slate-100">
               {/* <h4>Article Data</h4> */}
-              <p className="px-10 py-5 text-xl">{selectedArticle.Data}</p>
+              {/* <p className="px-10 py-5 text-xl">{selectedArticle.Data}</p>
             </div>
-          ) : null}
+          ) : null} */}
         </div>
       </div>
     </div>
